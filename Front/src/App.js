3753e76699b4e47eb317e9e7e2742b4a938db5b9
yank0vy3rdna <yankovyerdna@yandex.ru>
2021-10-14 04:26:16 +0300

    feat: added Home function

---
+++ Front/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Home() {
+  // @ts-ignore
+  return <div>
+
+    <h1>Shit code</h1>
+  </div>;
+}
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
