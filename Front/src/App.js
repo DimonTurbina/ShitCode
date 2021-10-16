2289cec72cca9ea6beb7c450a726a97475d9b3d4
d.elagin <dimonssturbo@mail.ru>
2021-10-16 17:23:20 +0300

    feat: add ShitPage

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,6 +10,40 @@ function Home() {
   </div>;
 }
 
+function ShitPage() {
+  let name;
+  ({name} = useParams());
+  const [data, setData] = useState({
+    "count_codes": 0,
+    "count_lines": 1,
+    "examples": [{
+      "id": 1,
+      "code": "",
+      "user_name": ""
+    }]
+  });
+  useEffect(() => {
+    fetch("/shitten_api/shits/" + name).then(r => r.json().then(data => {
+          setData(data)
+        })
+    )
+
+  }, [name]);
+  data['examples'].reverse()
+  // @ts-ignore
+  let k = data['examples'].map((object, i) => <div><h5>Экспонат номер {i}</h5>
+    <pre><code style={{"text-align": "left"}}>{object['code']}</code></pre>
+  </div>)
+
+  if (data['count_codes'] === 0)
+    k = <></>
+  return <div>
+    <h3>Автор хуйни: {name}</h3>
+    <h4>Количество говнокода: {data['count_codes']}</h4>
+    <h4>Количество строк говнокода: {data['count_lines']}</h4>
+    {k}
+  </div>;
+}
 
 
 function App() {
